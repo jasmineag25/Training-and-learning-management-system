8d56d5255ef8783561b2020cb9b69971b1a20fd1
Madhav Verma <madhavverma.hll@gmail.com>
2024-07-03 16:39:06 +0530

    Initial Commit

---
+++ EduMatrix Educator/EduMatrix_EducatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduMatrix_EducatorApp.swift
+//  EduMatrix Educator
+//
+//  Created by Madhav Verma on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct EduMatrix_EducatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
